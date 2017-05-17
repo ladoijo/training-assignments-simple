0785a59195664037de104b75ee8d2054b3595e95
ladoijo <ladoijo@gmail.com>
2017-05-17 16:14:19 +0700

    Test 1

--- src/java/eu/sig/training/ch03/FlagFactory.java
+++ src/java/eu/sig/training/ch03/FlagFactory.java
@@ -2,10 +2,26 @@ package eu.sig.training.ch03;
 
 import java.awt.Color;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FlagFactory {
 
+	private static Map<Nationality, List<Color>> FLAGS = new HashMap<Nationality, List<Color>>();
+	
+	/*static {
+		FLAGS.put(Nationality.DUTCH, Arrays.asList(Color.RED, Color.WHITE, Color.BLUE));
+		FLAGS.put(Nationality.GERMAN, Arrays.asList(Color.RED, Color.WHITE, Color.BLUE));
+		FLAGS.put(Nationality.BELGIAN, Arrays.asList(Color.RED, Color.WHITE, Color.BLUE));
+		FLAGS.put(Nationality.FRENCH, Arrays.asList(Color.RED, Color.WHITE, Color.BLUE));
+		FLAGS.put(Nationality.ITALIAN, Arrays.asList(Color.RED, Color.WHITE, Color.BLUE));
+		FLAGS.put(Nationality.ROMANIA, Arrays.asList(Color.RED, Color.WHITE, Color.BLUE));
+		FLAGS.put(Nationality.IRELAND, Arrays.asList(Color.RED, Color.WHITE, Color.BLUE));
+		FLAGS.put(Nationality.HUNGARIAN, Arrays.asList(Color.RED, Color.WHITE, Color.BLUE));
+		FLAGS.put(Nationality.BULGARIAN, Arrays.asList(Color.RED, Color.WHITE, Color.BLUE));
+		FLAGS.put(Nationality.RUSSIA, Arrays.asList(Color.RED, Color.WHITE, Color.BLUE));
+	}*/
     // tag::getFlag[]
     public List<Color> getFlagColors(Nationality nationality) {
         List<Color> result;
@@ -38,8 +54,6 @@ public class FlagFactory {
             result = Arrays.asList(Color.WHITE, Color.GREEN, Color.RED);
             break;
         case RUSSIA:
-            result = Arrays.asList(Color.WHITE, Color.BLUE, Color.RED);
-            break;
         case UNCLASSIFIED:
         default:
             result = Arrays.asList(Color.GRAY);
