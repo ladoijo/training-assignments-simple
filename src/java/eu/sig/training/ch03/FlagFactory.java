ee9d227db398cfe01a32380633ab9028b104be0e
ladoijo <ladoijo@gmail.com>
2017-05-17 17:31:42 +0700

    Test 5

--- src/java/eu/sig/training/ch03/FlagFactory.java
+++ src/java/eu/sig/training/ch03/FlagFactory.java
@@ -11,35 +11,36 @@ public class FlagFactory {
         List<Color> result;
         switch (nationality) {
         case DUTCH:
-            result = Arrays.asList(Color.RED, Color.WHITE, Color.BLUE);
-            break;
+            /*result = Arrays.asList(Color.RED, Color.WHITE, Color.BLUE);
+            break;*/
         case GERMAN:
-            result = Arrays.asList(Color.BLACK, Color.RED, Color.YELLOW);
-            break;
+            /*result = Arrays.asList(Color.BLACK, Color.RED, Color.YELLOW);
+            break;*/
         case BELGIAN:
-            result = Arrays.asList(Color.BLACK, Color.YELLOW, Color.RED);
-            break;
+            /*result = Arrays.asList(Color.BLACK, Color.YELLOW, Color.RED);
+            break;*/
         case FRENCH:
-            result = Arrays.asList(Color.BLUE, Color.WHITE, Color.RED);
-            break;
+            /*result = Arrays.asList(Color.BLUE, Color.WHITE, Color.RED);
+            break;*/
         case ITALIAN:
-            result = Arrays.asList(Color.GREEN, Color.WHITE, Color.RED);
-            break;
+           /* result = Arrays.asList(Color.GREEN, Color.WHITE, Color.RED);
+            break;*/
         case ROMANIA:
-            result = Arrays.asList(Color.BLUE, Color.YELLOW, Color.RED);
-            break;
+            /*result = Arrays.asList(Color.BLUE, Color.YELLOW, Color.RED);
+            break;*/
         case IRELAND:
-            result = Arrays.asList(Color.GREEN, Color.WHITE, Color.ORANGE);
-            break;
+            /*result = Arrays.asList(Color.GREEN, Color.WHITE, Color.ORANGE);
+            break;*/
         case HUNGARIAN:
-            result = Arrays.asList(Color.RED, Color.WHITE, Color.GREEN);
-            break;
+            /*result = Arrays.asList(Color.RED, Color.WHITE, Color.GREEN);
+            break;*/
         case BULGARIAN:
-            result = Arrays.asList(Color.WHITE, Color.GREEN, Color.RED);
-            break;
+            /*result = Arrays.asList(Color.WHITE, Color.GREEN, Color.RED);
+            break;*/
         case RUSSIA:
-            result = Arrays.asList(Color.WHITE, Color.BLUE, Color.RED);
-            break;
+            /*result = Arrays.asList(Color.WHITE, Color.BLUE, Color.RED);
+            break;*/
+        case UNCLASSIFIED:
         default:
             result = Arrays.asList(Color.GRAY);
             break;
